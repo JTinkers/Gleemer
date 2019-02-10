424d836ed144d26966bb16eb6288e514a8d611b4
JonekCode <jonkujanek@gmail.com>
2019-02-11 00:12:15 +0100

    Added margin generator

---
+++ Gleemer/resources/views/layouts/default.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/core.min.css">
+        <title>@section('title') - Gleemer</title>
+    </head>
+    <body>
+        @nav
+        @yield('content')
+        @footer
+    </body>
+</html>
