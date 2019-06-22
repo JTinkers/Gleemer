7a6e1156a02ed41386c6ff609e67ac5d1343fe87
JonekCode <jonkujanek@gmail.com>
2019-06-23 01:53:29 +0200

    Added russian translation

---
+++ Gleemer/resources/lang/ru/general.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'credits' => 'Создан при помощи <i class="fas fa-heart"></i> вызван <i class="fas fa-coffee"></i>',
+];
---
+++ Gleemer/resources/lang/ru/snippets.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'comments' => 'Комментарии',
+    'ratings' => 'Оценки',
+    'rating' => 'Оценка',
+    'views' => 'Просмотры',
+    'favourites' => 'Избранное',
+    'select_visibility' => 'Выберите видимость',
+    'select_language' => 'Выбор языка',
+	'toolbar_copy' => 'Копировать',
+	'toolbar_share' => 'Поделиться',
+	'toolbar_save' => 'Сохранить'
+];
